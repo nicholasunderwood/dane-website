e67b81ab3e3acf6ef6cf0303c1cb0bf15184c408
Nicholas Underwood <nachos.underwood@gmail.com>
2023-01-13 17:39:37 -0800

    buttons also working

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function optimizeGrid(width, height, numCells){
-    console.log(width, height, numCells)
+    // console.log(width, height, numCells)
     for(let cols = numCells; cols > 0; cols--){
         let p_rows = Math.ceil(numCells / cols);
         let p_height = width / cols * cellRatio * p_rows
         
-        console.log(p_height, height, cols, cellRatio)
+        // console.log(p_height, height, cols, cellRatio)
         if(p_height > height){
             return cols+1;
         }
@@ -21,7 +21,7 @@ function loadGrid(){
     const win = $('#content');
     const w = win.width();
     const h = win.height();
-    console.log('h: ' + h);
+    // console.log('h: ' + h);
     // return;
     cols = optimizeGrid(w,h,numCells)
     console.log('cols: ' + cols);
@@ -44,7 +44,8 @@ function loadGrid(){
         win.append(cell);
     }
 
-    win.css('flex', '0 1 auto')
+    win.css('flex', '0 1 auto');
+
 
 }
 
@@ -80,22 +81,23 @@ const projects = [];
 const imgs = ['IMG_0667', 'IMG_0668', 'IMG_0671', 'IMG_0672', 'IMG_0674', 'IMG_0675', 'IMG_0678', 'IMG_0680']
 
 
-$('#filter .btn').click((e) => {
+$('#filter button').click((e) => {
+    console.log(e.target.id);
     $(e.target).addClass('btn-secondary').removeClass('btn-outline-secondary')
         .siblings().removeClass('btn-secondary').addClass('btn-outline-secondary');
 
     if(e.target.id === '4') {
-        $('#content table td').removeClass('hidden');
+        $('#content .cell').removeClass('hidden');
         return;    
     };
 
-    $('#content table td').addClass('hidden');
+    $('#content .cell').addClass('hidden');
     if(e.target.id === '1'){
-        $('#content table td.video').removeClass('hidden');
+        $('#content .cell.video').removeClass('hidden');
     } else if(e.target.id === '2') {
-        $('#content table td.audio').removeClass('hidden');
+        $('#content .cell.audio').removeClass('hidden');
     } else if(e.target.id === '3') {
-        $('#content table td.episode').removeClass('hidden');
+        $('#content .cell.episode').removeClass('hidden');
     }
 });
 
@@ -107,18 +109,22 @@ $(window).resize(() => {
 
 
 setTimeout(() => {
-    loadGrid()
+    loadGrid();
+
+    console.log($('#content .cell'));
+
+    $('#content .cell').each((i,e) => {
+        e = $(e);
+        if(i%7 === 0 || i%7 === 3 || i%5 === 0) {
+            e.addClass('video');
+        } else if(i%4 === 0 || i%4 === 2) {
+            e.addClass('audio');
+        } else {
+            e.addClass('episode');
+        }
+    
+    });
+    
 }, 1);
 // loadGrid();
 
-$('#content table td').each((i,e) => {
-
-    if(i%7 === 0 || i%7 === 3 || i%5 === 0) {
-        e.addClass('video');
-    } else if(i%4 === 0 || i%4 === 2) {
-        e.addClass('audio');
-    } else {
-        e.addClass('episode');
-    }
-
-});
